3b0e6199929924ef8c6aba1fb8090eaf204ad9b6
Danielle Davis <danielledavis@danielles-mbp.home>
2022-07-30 19:08:27 +0100

    changes to everything

---
+++ src/components/SearchResults.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/search-results.css";
+
+const SearchResults = ({ results }) => {
+  if (!results.length) {
+    return <p>No results</p>;
+  } else {
+    return (
+      <>
+        {results.map((image) => (
+          <img className="card-image" src={image} alt="spaceImage" />
+        ))}
+      </>
+    );
+  }
+};
+
+export default SearchResults;
